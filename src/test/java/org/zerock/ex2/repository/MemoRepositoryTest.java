a0d998b4aa069bd3b55a738d05709ed5419f3965
SYU <xeungjin@gmail.com>
2025-03-13 15:28:50 +0900

    testSort

--- src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
+++ src/test/java/org/zerock/ex2/repository/MemoRepositoryTest.java
@@ -3,6 +3,10 @@ package org.zerock.ex2.repository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 import org.zerock.ex2.entity.Memo;
 
@@ -62,5 +66,34 @@ public class MemoRepositoryTest {
         Long mno = 100L;
 
         memoRepository.deleteById(mno);
+    }
+
+    @Test
+    public void testPageDefault() {
+        Pageable pageable = PageRequest.of(1, 10);
+        Page<Memo> result = memoRepository.findAll(pageable);
+        System.out.println(result);
+        System.out.println("-------------------------");
+        System.out.println("Total Pages: " + result.getTotalPages());
+        System.out.println("Total Count : " + result.getTotalElements());
+        System.out.println("Page Number : " + result.getNumber());
+        System.out.println("Page Size : " + result.getSize());
+        System.out.println("hash next page? : " + result.hasNext());
+        System.out.println("first page? : " + result.isFirst());
+        System.out.println("-------------------------");
+        for(Memo memo : result.getContent()) {
+            System.out.println(memo);
+        }
+    }
+
+    @Test
+    public void testSort() {
+        Sort sort1 = Sort.by("mno").descending();
+        Pageable pageable = PageRequest.of(0, 10, sort1);
+        Page<Memo> result = memoRepository.findAll(pageable);
+
+        result.get().forEach(memo->{
+            System.out.println(memo);
+        });
     }
 }
